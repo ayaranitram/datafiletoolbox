405acb276bed85792d7174bcc3a1feed59b6b1b9
Martín Araya <martinaraya@gmail.com>
2020-04-01 10:05:49 +0200

    cleaning

--- loadBMS.py
+++ loadBMS.py
@@ -9,16 +9,8 @@ import datafiletoolbox as dftb
 from datafiletoolbox import expandKeyword
 BMS = dftb.simObject.Simulation()
 BMS_HM = BMS.LoadModelFromData('C:/Users/mcaraya/OneDrive - Cepsa/git/sampleData/bms/HM_AZ45_E_1/HM_AZ45_E_1.DATA')
-SATNUM = BMS[1]['SATNUM'].args
-
-# keywordArgument = SATNUM
-# if '/' in keywordArgument :
-#     keywordArgument = keywordArgument[ : keywordArgument.index('/') ]
-# keywordArgument = keywordArgument.replace("\n"," ")
-# keywordArgument = expandKeyword(keywordArgument)
-
-
+BMS[1]['SATNUM'].args
         
 BMS[1]['SATNUM'].arg2prop()
-BMS[1]['SATNUM'].prop
+SATNUM = BMS[1]['SATNUM'].prop
 BMS[1]['SATNUM'].prop.shape
