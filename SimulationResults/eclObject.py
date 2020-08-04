ef418521340433c99f641cf576065e03ef659ae3
martinaraya <martinaraya@gmail.com>
2020-08-04 14:48:56 +0200

    delete equinor copy from datafiletoolbox, use ecl from pypi

--- SimulationResults/eclObject.py
+++ SimulationResults/eclObject.py
@@ -14,14 +14,16 @@ from datafiletoolbox.common.inout import verbose
 import numpy as np
 import os
 
-eclPath = extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/lib/python' 
-os.environ['PYTHONPATH'] = eclPath + ';' + os.environ['PYTHONPATH']
-eclPath = eclPath + ';' + extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/lib'
-eclPath = eclPath + ';' + extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/bin'
-os.environ['PATH'] = eclPath + ';' + os.environ['PATH']
-from datafiletoolbox.equinor.libecl.win10.lib.python import ecl
-from datafiletoolbox.equinor.libecl.win10.lib.python.ecl import summary
-           
+#eclPath = extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/lib/python' 
+#os.environ['PYTHONPATH'] = eclPath + ';' + os.environ['PYTHONPATH']
+#eclPath = eclPath + ';' + extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/lib'
+#eclPath = eclPath + ';' + extension(str(os.getcwd()))[3] + '/datafiletoolbox/equinor/libecl/win10/bin'
+#os.environ['PATH'] = eclPath + ';' + os.environ['PATH']
+
+#from datafiletoolbox.equinor.libecl.win10.lib.python import ecl
+import ecl
+#from datafiletoolbox.equinor.libecl.win10.lib.python.ecl import summary
+       
 class ECL(SimResult):
     """
     object to contain eclipse format results read from SMSPEC using libecl from equinor 
--- equinor/libecl/win10/lib/python/ecl/__ecl_lib_info.py
+++
@@ -1,9 +0,0 @@
-
-
-class EclLibInfo(object):
-  lib_path = "../../../../lib"
-  so_version = ""
-  __version__ = "0.0.not-available"
-
-  def __init__(self):
-    pass
