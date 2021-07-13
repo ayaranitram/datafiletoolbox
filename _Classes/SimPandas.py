342b0559742b48f4236d207fd81c0bb1f8292c6a
Martín Carlos Araya <martinaraya@gmail.com>
2021-07-13 22:56:15 +0200

    thinking about restructuring .yearly method

--- _Classes/SimPandas.py
+++ _Classes/SimPandas.py
@@ -2779,9 +2779,9 @@ Copy of input object, shifted.
             std : returns the standard deviation per year
             sum : returns the summation of all the values per year
             count : returns the number of rows per year
-            int or integrate : calculates the numerical integration over the index (a datetime-index) and returns 
-            repr or representative : calculates the numerical integration of the column over the index (a datetime-index) and then divide it by the elapsed time on between each pair of rows
-            cum or cumulative : first run cumsum over the columns and then return the last value of each year
+            integrate : calculates the numerical integration over the index (a datetime-index) and returns 
+            representative : calculates the numerical integration of the column over the index (a datetime-index) and then divide it by the elapsed time on between each pair of rows
+            cumsum or cumulative : first run cumsum over the columns and then return the last value of each year
         """
         try :
             result = self.DF.groupby(self.index.year)
@@ -2805,7 +2805,7 @@ Copy of input object, shifted.
             result = result.sum()
         elif outBy == 'count' :
             result = result.count()
-        elif outBy in ['int', 'integrate', 'integral', 'cum', 'cumulative', 'representative','rep'] :
+        elif outBy in ['int', 'integrate', 'integral', 'cum', 'cumulative', 'representative','rep','repr'] :
             result = self.integrate()
             result = result.DF.groupby(self.index.year)
             index = DataFrame(data=self.index, index=self.index).groupby(self.index.year)
