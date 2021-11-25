b05ae985dfcd652ac60c208dafe86dc39db503b3
Martín Carlos Araya <martinaraya@gmail.com>
2021-11-25 11:40:52 +0100

    messange corrected when not able to import cwrap

--- _Classes/Errors.py
+++ _Classes/Errors.py
@@ -4,7 +4,7 @@ Created on Wed May 13 15:14:35 2020
 @author: MCARAYA
 """
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __release = 210924
 __all__ = []
 
@@ -21,7 +21,7 @@ class UndefinedDateFormat(Exception):
 try :
     from cwrap import PrototypeError
 except ModuleNotFoundError :
-    print("\n WARNING: missing 'cwrap' module, will be able to load eclipse style simulation outputs.\n         Please intall it using pip command:\n           pip install cwrap\n\n       or upgrade:\n\n          pip install cwrap --upgrade\n        or intall libecl using pip command:\n           pip install libecl\n\n       or upgrade:\n\n          pip install libecl --upgrade" )
+    print("\n WARNING: missing 'cwrap' module, will not be able to load eclipse style simulation outputs.\n         Please intall it using pip command:\n           pip install cwrap\n\n       or upgrade:\n\n          pip install cwrap --upgrade\n        or intall libecl using pip command:\n           pip install libecl\n\n       or upgrade:\n\n          pip install libecl --upgrade" )
 
 
 class MissingDependence(Exception):
