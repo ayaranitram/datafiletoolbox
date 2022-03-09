48b96133a7bdde19c2453b1485256cd7995bd4b9
Martín Carlos Araya <martinaraya@gmail.com>
2022-03-09 19:39:31 +0100

    read_excel function

--- _Classes/SimPandas.py
+++ _Classes/SimPandas.py
@@ -6,9 +6,9 @@ Created on Sun Oct 11 11:14:32 2020
 @author: martin
 """
 
-__all__ = ['SimSeries', 'SimDataFrame']
 __version__ = '0.75.0'
 __release__ = 220309
+__all__ = ['SimSeries', 'SimDataFrame', 'read_excel']
 
 from sys import getsizeof
 from io import StringIO
@@ -44,6 +44,114 @@ _SERIES_WARNING_MSG = """\
     You are passing unitless data to the SimSeries constructor. Currently,
     it falls back to returning a pandas Series. But in the future, we will start
     to raise a TypeError instead."""
+
+
+def read_excel(io, sheet_name=None, header=0, names=None, index_col=None, usecols=None, squeeze=None, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skiprows=None, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, parse_dates=False, date_parser=None, thousands=None, decimal='.', comment=None, skipfooter=0, convert_float=None, mangle_dupe_cols=True, storage_options=None,
+units=1, speak=False, indexName=None, indexUnits=None, nameSeparator=None, intersectionCharacter='∩', autoAppend=False, transposed=False, operatePerName=False, *args, **kwargs):
+    """
+    wrapper of pandas.read_excel enhanced with units support
+
+    """
+    import pandas
+
+    dateunits = ['date']  #,'fecha']
+    speak = bool(speak)
+
+    if type(units) is int:
+        if units < 0:
+            raise ValueError("'units' parameter must be positive")
+        if type(header) is int:
+            if header == units:
+                if speak:
+                    print(" > same row will be used as header and as units.")
+            else:
+                header = [header, units]
+        elif type(header) is list:
+            if len(header) == 1 and units in header:
+                if speak:
+                    print(" > same row will be used as header and as units.")
+                header = header[0]
+            else:
+                header += [units]
+
+    excelread = pandas.read_excel(io, sheet_name=sheet_name, header=header, names=names, index_col=index_col, usecols=usecols, squeeze=False, dtype=dtype, engine=engine, converters=converters, true_values=true_values, false_values=false_values, skiprows=skiprows, nrows=nrows, na_values=na_values, keep_default_na=keep_default_na, na_filter=na_filter, verbose=verbose, parse_dates=parse_dates, date_parser=date_parser, thousands=thousands, comment=comment, skipfooter=skipfooter, mangle_dupe_cols=mangle_dupe_cols, storage_options=storage_options)  # convert_float=convert_float, decimal=decimal
+
+    if type(excelread) is not dict:
+        excelread = {'onesheet':excelread}
+
+    output = {}
+    for name,df in excelread.items():
+
+        if type(units) is list:
+            if len(units) == len(df.columns):
+                dataunits = {df.columns[i]:units[i] for i in len(units)}
+            else:
+                raise ValueError("if 'units' is a list, it must be same length as the columns of the dataframe")
+        elif type(units) is str:
+            dataunits = units
+        elif type(units) is int:
+            if type(header) is list:
+                if len(header) == 2:
+                    dataunits = {}
+                    newcols = []
+                    for col in df.columns:
+                        if str(col[-1]).startswith('Unnamed:'):
+                            nc = str(col[0]).strip()
+                            dataunits[nc] = 'unitless'
+                            newcols.append(nc)
+                        else:
+                            nc = str(col[0]).strip()
+                            dataunits[nc] = str(col[-1]).strip()
+                            newcols.append(nc)
+                elif len(header) > 2:
+                    dataunits = {}
+                    newcols = []
+                    for col in df.columns:
+                        if str(col[-1]).startswith('Unnamed:'):
+                            nc = col[:-1]
+                            dataunits[nc] = 'unitless'
+                            newcols.append(nc)
+                        else:
+                            nc = col[:-1]
+                            dataunits[nc] = str(col[-1]).strip()
+                            newcols.append(nc)
+                    newcols = pandas.MultiIndex.from_tuples(newcols)
+                df.columns = newcols
+            elif type(header) is int:
+                dataunits = {c:str(c) for c in df.columns}
+
+        elif units is None:
+            dataunits = None
+        else:
+            dataunits = units
+
+        if isinstance(df,pandas.DataFrame):
+            for col in df.columns:
+                if str(df[col].dtype).startswith('date'):
+                    if type(dataunits) is not dict:
+                        dataunits = {c:dataunits for c in df.columns}
+                    if col in dataunits:
+                        if str(dataunits[col]).lower().strip() not in dateunits:
+                            dataunits[col] = 'date'
+                    else:
+                        dataunits[col] = 'date'
+        elif isinstance(df,pandas.Series):
+            if str(df.dtype).startswith('date'):
+                if df.name is not None:
+                    dataunits = {df.name:'date'}
+        if str(df.index.dtype).startswith('date'):
+            if str(indexUnits).lowrt().strip() not in dateunits:
+                indexUnits = 'date'
+
+        output[name] = SimDataFrame(data=df, units=dataunits, speak=speak, indexName=indexName, indexUnits=indexUnits, nameSeparator=nameSeparator, intersectionCharacter=intersectionCharacter, autoAppend=autoAppend, transposed=transposed, operatePerName=operatePerName, *args, **kwargs)
+
+        if bool(squeeze):
+            output[name] = output[name].squeeze()
+
+    if len(output) == 1:
+        return output[name]
+    else:
+        return output
 
 
 def _simseries_constructor_with_fallback(data=None, index=None, units=None, **kwargs):
@@ -2972,7 +3080,7 @@ Copy of input object, shifted.
             The projection after squeezing axis or all the axes. and units
 
         """
-        if len(self.columns) == 1 or len(self.index) == 1::
+        if len(self.columns) == 1 or len(self.index) == 1:
             return self.to_SimSeries().squeeze()
         elif len(self.get_Units()) == 0 or np.array([(u is None or str(u).lower().strip() in ['unitless','dimensionless']) for u in self.get_Units().values()]).all():
             return self.as_DataFrame()
--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ Created on Tue Jul  9 21:23:27 2019
 @author: martin
 """
 
-__version__ = '0.46.2'
-__release__ = 220223
+__version__ = '0.48.2'
+__release__ = 220309
 __author__ = 'Martin Carlos Araya <martinaraya@gmail.com>'
 __all__ = ['stringformat', 'extension', 'Alternate', 'convert', 'SimPandas',
            'SimSeries', 'SimDataFrame', 'loadSimulationResults','melt','pivot','unify','slope']
@@ -19,7 +19,7 @@ _msg = """     implementing libecl 2.9.1 from pypi, released on Aug 2020"""
 
 
 from ._Classes.Iterators import Alternate
-from ._Classes.SimPandas import SimSeries, SimDataFrame
+from ._Classes.SimPandas import SimSeries, SimDataFrame, read_excel
 from ._Classes import SimPandas
 
 from ._common.eclDATES import simDate as ECLdate
