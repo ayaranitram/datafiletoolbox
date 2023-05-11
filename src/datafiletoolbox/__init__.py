23578df63d3edb79c236ae6f3bfefff764abf1d1
Martín Araya <martinaraya@gmail.com>
2023-05-12 00:07:37 +0200

    new built

--- src/datafiletoolbox/__init__.py
+++ src/datafiletoolbox/__init__.py
@@ -9,8 +9,8 @@ Created on Tue Jul  9 21:23:27 2019
 @author: martin
 """
 
-__version__ = '0.52.36'
-__release__ = 20230425
+__version__ = '0.52.38'
+__release__ = 20230511
 __author__ = 'Martin Carlos Araya <martinaraya@gmail.com>'
 __all__ = ['stringformat', 'extension', 'Alternate', 'convert', 'SimPandas',
            'SimSeries', 'SimDataFrame', 'loadSimulationResults', 'melt', 'pivot', 'unify', 'slope']
