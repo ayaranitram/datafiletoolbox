ee8510dba1ed77dbdb54a4f5cd37246c0538ac2a
Martín Carlos Araya <52510177+ayaranitram@users.noreply.github.com>
2024-04-27 22:31:20 +0200

    Update version number

--- src/datafiletoolbox/__init__.py
+++ src/datafiletoolbox/__init__.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Data File Tool Box is a set of routines useful to work with eclipse style
-data files and properties.
+Data File Tool Box is a set of routines useful to work with eclipse-style data files and properties include files.
 
 Created on Tue Jul  9 21:23:27 2019
 @author: martin
 """
 
-__version__ = '0.52.40'
-__release__ = 20230619
+__version__ = '0.52.42'
+__release__ = 20240425
 __author__ = 'Martin Carlos Araya <martinaraya@gmail.com>'
 __all__ = ['stringformat', 'extension', 'Alternate', 'convert', 'SimPandas',
            'SimSeries', 'SimDataFrame', 'loadSimulationResults', 'melt', 'pivot', 'unify', 'slope']
